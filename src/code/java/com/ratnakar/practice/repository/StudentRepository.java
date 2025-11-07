1b609f00e81271c91be091f593a97b5967f8c203
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-11-07 17:59:24 +0530

    H2 Database Configuration

--- src/code/java/com/ratnakar/practice/repository/StudentRepository.java
+++ src/code/java/com/ratnakar/practice/repository/StudentRepository.java
@@ -1,6 +1,8 @@
 package com.ratnakar.practice.repository;
 
 import com.ratnakar.practice.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,8 +10,20 @@ import java.util.List;
 
 @Repository
 public class StudentRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student student){
-        System.out.println("Student Added Successfully .......");
+        String sqlQuery = "insert into student (rollNo, name, marks) values (?,?,?)";
+        int rows = jdbcTemplate.update(sqlQuery, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows+" Student Added Successfully .......");
     }
 
     public List<Student> findAll() {
---
+++ src/code/resources/H2Database/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollNo, name, marks) values (201, 'Kiran', 79);
+insert into student (rollNo, name, marks) values (202, 'Navin', 67);
+insert into student (rollNo, name, marks) values (203, 'Shreya', 81);
