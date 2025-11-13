2eedf4752288c29dbd3fea869de811819dc65cfa
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-11-13 16:51:41 +0530

    Servlet Mapping

--- src/test/java/com/practice/ratnakar/ServletApplication.java
+++ src/test/java/com/practice/ratnakar/ServletApplication.java
@@ -1,13 +1,24 @@
 package com.practice.ratnakar;
 
+import com.practice.ratnakar.servlet.TestServlet;
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
 public class ServletApplication {
     public static void main(String[] args) throws LifecycleException {
         // To call the servlet we have to start the Tomcat Server, for that we need to call the Tomcat
-        System.out.println("Calling the Tomcat Server ................");
         Tomcat tomcat = new Tomcat();
+        System.out.println("Calling the Tomcat Server ................");
+        // Setting the http Port for tomcat
+        tomcat.setPort(8091);
+        // Now to do the mapping of our servlet we have to call the context of that class
+        Context servletContext = tomcat.addContext("", null); // default configuration of servlet context
+        // Calling the TestServlet class
+        Tomcat.addServlet(servletContext, "TestServlet", new TestServlet());
+        // Now we will do the actual Mapping of Servlet
+        // {/greetings/hello} this endpoint is defined in the TestServlet
+        servletContext.addServletMappingDecoded("/greetings/hello", "TestServlet");
         // Starting the server
         tomcat.start();
         // By default Tomcat server will stop running after few seconds, once it is invoked
--- src/test/java/com/practice/ratnakar/servlet/TestServlet.java
+++ src/test/java/com/practice/ratnakar/servlet/TestServlet.java
@@ -1,16 +1,28 @@
 package com.practice.ratnakar.servlet;
 
+
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/greetings/hello")
 public class TestServlet extends HttpServlet {
 
     // Servlet is basically used when we have to interact with FrontEnd User Interface client and backend server client
     // Here we are receiving data from front end UI/Mobile App in the form of HTTP request & We are sending that data after some intermediate processing to the backend server
     // Then we are returning the server response and sending the response back to the front end through HTTP response
     // For above use case we are using the two interfaces from "jakarta.servlet" called HttpServletRequest & HttpServletResponse
-    public void service(HttpServletRequest req, HttpServletResponse res){
-
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("Initializing the Servlet .....................");
+        // Setting the type of data which is getting published in response object of servlet
+        res.setContentType("text/html");
+        // Sending response to the Web Client or Mobile Client
+        PrintWriter out = res.getWriter(); // Writing some content on response object with getWriter() method
+        out.println("<h2><b>Hello World !</b></h2>"); // Writing our Message with HTML Tags for this we have to set the type of data for our web client in the response object
     }
 }
